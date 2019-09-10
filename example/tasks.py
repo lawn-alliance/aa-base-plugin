0532d2e1ab954d726d6e782a335aaed2fb6d915d
ErikKalkoken <erik.kalkoken@gmail.com>
2019-09-10 13:15:55 +0200

    add tasks template

---
+++ example/tasks.py
@@ -0,0 +1,15 @@
+from celery import shared_task
+import logging
+
+logger = logging.getLogger(__name__)
+
+# Create your tasks here
+
+"""
+Example task:
+
+@shared_task
+def my_task():
+    pass
+"""
+   
